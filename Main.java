555663a68d8a186a0533275daa68b66bf50155e7
Tyla Jeffs <tylajeffs@gmail.com>
2023-01-06 10:42:41 -1000

    add documentation and menu bar

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 
-public class Main extends JFrame {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame implements ActionListener{
 
     private Vis mainPanel;
 
@@ -24,15 +27,29 @@ public class Main extends JFrame {
         new Main();
     }
 
-
+    /**
+     * method to create a menu bar
+     * @return the menu bar
+     */
     private JMenuBar createMenu() {
+        //observer design pattern
+
+        //create menu
         JMenuBar mb = new JMenuBar();
         JMenu file = new JMenu("File");
         JMenu edit = new JMenu("Edit");
+
+        //create submenus
         JMenuItem a = new JMenuItem("a");
+        a.addActionListener(this);
         JMenuItem b = new JMenuItem("b");
+        b.addActionListener(this);
         JMenuItem c = new JMenuItem("c");
+        c.addActionListener(this);
         JMenuItem d = new JMenuItem("d");
+        d.addActionListener(this);
+
+        //tie everything together
         file.add(a);
         file.add(b);
         edit.add(c);
@@ -40,5 +57,11 @@ public class Main extends JFrame {
         mb.add(file);
         mb.add(edit);
         return mb;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 }
